48b106cdd02c2a66de1e88462ec64a88f345dc11
huckjoo <huckjoo96@naver.com>
2022-05-14 21:59:56 +0900

    feat: implement loading UI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { request } from '../api/index.js';
 import Breadcrumb from './components/Breadcrumb.js';
 import ImageView from './components/ImageView.js';
 import Nodes from './components/Nodes.js';
+import Loading from './components/Loading.js';
 
 export default function App($app) {
   this.state = {
@@ -9,8 +10,10 @@ export default function App($app) {
     nodes: [],
     depth: [],
     selectedFilePath: null,
+    isLoading: false,
   };
   const breadcrumb = new Breadcrumb({ $app, initialState: this.state.depth });
+  const loading = new Loading({ $app, initialState: this.state.isLoading });
   const imageView = new ImageView({
     $app,
     initialState: this.state.selectedFilePath,
@@ -25,8 +28,9 @@ export default function App($app) {
     initialState: { isRoot: this.state.isRoot, nodes: this.state.nodes },
     onClick: async (node) => {
       if (node.type === 'DIRECTORY') {
+        this.setState({ ...this.state, isLoading: true });
         const nextNodes = await request(node.id);
-        this.setState({ ...this.state, isRoot: false, nodes: nextNodes, depth: [...this.state.depth, node] });
+        this.setState({ ...this.state, isRoot: false, nodes: nextNodes, depth: [...this.state.depth, node], isLoading: false });
       } else if (node.type === 'FILE') {
         imageView.setState({
           ...this.state,
@@ -39,11 +43,13 @@ export default function App($app) {
         const nextState = { ...this.state };
         nextState.depth.pop();
         const prevNodeId = nextState.depth.length ? nextState.depth[nextState.depth.length - 1].id : null;
+        this.setState({ ...this.state, isLoading: true });
         this.setState({
           ...nextState,
           isRoot: !prevNodeId,
           nodes: await request(prevNodeId),
         });
+        this.setState({ ...this.state, isLoading: false });
       } catch (e) {
         throw new Error(e.message);
       }
@@ -55,11 +61,14 @@ export default function App($app) {
     breadcrumb.setState(this.state.depth);
     nodes.setState({ isRoot: this.state.isRoot, nodes: this.state.nodes });
     imageView.setState(this.state.selectedFilePath);
+    loading.setState(this.state.isLoading);
   };
 
   const init = async () => {
     try {
+      this.setState({ ...this.state, isLoading: true });
       const rootNodes = await request();
+      this.setState({ ...this.state, isLoading: false });
       window.addEventListener('keydown', (e) => {
         if (e.key === 'Escape') {
           this.setState({ ...this.state, selectedFilePath: null });
